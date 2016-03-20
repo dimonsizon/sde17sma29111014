f8a8b6dae70aba3d88d8a38a250e97ee7e1e18eb
Dmitry Sizon <dimonsizon@mail.ru>
2016-03-20 19:58:01 +0300

    Added directive for filters. Added default image.

--- web/js/directives.js
+++ web/js/directives.js
@@ -1,11 +1,29 @@
 ﻿var appDirectives = angular.module('app.directives', []);
 
+appDirectives.directive('productFilters', [function () {
+    return {
+        restriction: 'E',
+        templateUrl: "/views/directives-template/product-filters-template.html",
+        scope: {
+            productsList: '=',
+            rootScope: '='
+        },
+        link: function (scope, element, attrs) {
+            
+        },
+        controller: function ($scope, $element, $attrs, $rootScope) {
+
+        }
+    }
+}]);
+
 appDirectives.directive('productList', [function () {
     return {
         restriction: 'E',
         templateUrl: "/views/directives-template/product-list-template.html",
         scope: {
-            productsList: '='
+            productsList: '=',
+            productFilter: '='
         },
         link: function (scope, element, attrs) {
 
