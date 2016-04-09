3371eee6c161632d77370d37bf94544c7b617c0f
Dmitry Sizon <dimonsizon@mail.ru>
2016-04-09 14:38:44 +0300

    Implemented product details page. Added new styles.

--- web/views/products/product-details.js
+++ web/views/products/product-details.js
@@ -13,7 +13,9 @@ angular.module('app.productDetails', ['ngRoute'])
         DetailsServices.get({ gasId: $stateParams.gasId }, function (product) {
             $scope.productList = product.product;
             for (var i = 0; i < $scope.productList.length; i++) {
-                $scope.productList[i].images = $scope.productList[i].images.split(','); //images string to array
+                $scope.productList[i].images = $scope.productList[i].images.split(',');         //images string to array
+                $scope.productList[i].color = $scope.productList[i].color.split(',');           //color string to array
+                $scope.productList[i].secondType = $scope.productList[i].secondType.split(','); //secondType string to array
             }
             
             $scope.setProduct(0); //show first product
