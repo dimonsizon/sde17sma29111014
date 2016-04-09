4680c76a9c7d8a5cb42d485d02ec92f713464088
Dmitry Sizon <dimonsizon@mail.ru>
2016-04-09 12:55:35 +0300

    Changed logic for product details filter.

--- web/views/products/product-details.js
+++ web/views/products/product-details.js
@@ -4,18 +4,25 @@ angular.module('app.productDetails', ['ngRoute'])
 
 .controller('ProductDetailsCtrl', ['$scope', '$http', '$rootScope', '$stateParams', 'DetailsServices',
     function ($scope, $http, $rootScope, $stateParams, DetailsServices) {
+        $scope.productList = [];
         $scope.product = [];
         $scope.showProductIndex = 0;
         $scope.productLoading = true;
 
 
         DetailsServices.get({ gasId: $stateParams.gasId }, function (product) {
-            $scope.product = product.product;
-            for (var i = 0; i < $scope.product.length; i++) {
-                $scope.product[i].images = $scope.product[i].images.split(','); //images string to array
+            $scope.productList = product.product;
+            for (var i = 0; i < $scope.productList.length; i++) {
+                $scope.productList[i].images = $scope.productList[i].images.split(','); //images string to array
             }
             
+            $scope.setProduct(0); //show first product
             $scope.productLoading = false;
             //imgMassiv = $scope.service.images;
         });
+
+        $scope.setProduct = function (index) {
+            $scope.product = $scope.productList[index];
+            $scope.showProductIndex = index;
+        }
     }]);
