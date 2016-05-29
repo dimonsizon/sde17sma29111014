e0db6ad57edb1e2e4ed1adddfdfaf11596584e2b
Dmitry Sizon <dimonsizon@mail.ru>
2016-05-29 17:12:09 +0300

    Added new logic for editor.

--- web/views/editor/editor.js
+++ web/views/editor/editor.js
@@ -27,10 +27,7 @@ angular.module('app.editor', ['ngRoute'])
                 date = $filter('date')(new Date().getTime(), 'dd-MM-yy_HH-mm-ss');
 
             fileItem.file.name = date + extension;
-            fileItem.upload(); //auto upload after additing
-            if (!$scope.selectedImg) {
-                $scope.setImgToEditor(fileItem); //add first img to editor
-            }
+            fileItem.upload(); //auto upload after additing            
 
             console.info('onAfterAddingFile', fileItem);
         };
@@ -41,7 +38,7 @@ angular.module('app.editor', ['ngRoute'])
             console.info('onBeforeUploadItem', item);
         };
         uploader.onProgressItem = function (fileItem, progress) {
-            console.info('onProgressItem', fileItem, progress);
+            console.info('onProgressItem', fileItem, progress);            
         };
         uploader.onProgressAll = function (progress) {
             console.info('onProgressAll', progress);
@@ -56,9 +53,13 @@ angular.module('app.editor', ['ngRoute'])
             console.info('onCancelItem', fileItem, response, status, headers);
         };
         uploader.onCompleteItem = function (fileItem, response, status, headers) {
+            if (!$scope.selectedImg) {
+                $scope.setImgToEditor(fileItem); //add first img to editor
+            }
+
             console.info('onCompleteItem', fileItem, response, status, headers);
         };
-        uploader.onCompleteAll = function () {
+        uploader.onCompleteAll = function () {            
             console.info('onCompleteAll');
         };
 
@@ -131,5 +132,28 @@ angular.module('app.editor', ['ngRoute'])
         }
 
         $scope.editor.init();
+
+        $scope.productMockup = {
+            "case": {
+                "threeD": [
+                    { "id": "iphone4", "name": "iPhone 4/4S", "url": "/images/mockups/case/3d/mockup_iphone4.png" },
+                    { "id": "iphone5", "name": "iPhone 5/5S/5SE", "url": "/images/mockups/case/3d/mockup_iphone5.png"},
+                    { "id": "iphone5c", "name": "iPhone 5C", "url": "/images/mockups/case/3d/mockup_iphone5c.png" },
+                    { "id": "iphone6", "name": "iPhone 6/6S", "url": "/images/mockups/case/3d/mockup_iphone6.png" },
+                    { "id": "iphone6plus", "name": "iPhone 6 plus", "url": "/images/mockups/case/3d/mockup_iphone6plus.png" },
+                    { "id": "samsungS3", "name": "Samsung S3", "url": "/images/mockups/case/3d/mockup_samsungS3.png" },
+                    { "id": "samsungS4", "name": "Samsung S4", "url": "/images/mockups/case/3d/mockup_samsungS4.png" },
+                    { "id": "samsungS5", "name": "Samsung S5", "url": "/images/mockups/case/3d/mockup_samsungS5.png" },
+                    { "id": "samsungS5mini", "name": "Samsung S5 mini", "url": "/images/mockups/case/3d/mockup_samsungS5mini.png" },
+                ],
+                "twoD": [
+             
+                ]
+            }
+        }
+
+        $scope.setMackup = function (item) {
+            $scope.mockupImg = item.url;
+        }
 
     }]);
